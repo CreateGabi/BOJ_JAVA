6ce31a432f987059c17a4214a69086d9c1e52b15
Hyeonyeong <105722709+CreateGabi@users.noreply.github.com>
2022-10-26 23:34:58 +0900

    Add files via upload

--- BOJ/S10972.java
+++ BOJ/S10972.java
@@ -11,6 +11,7 @@ public class S10972 {
 		Scanner sc = new Scanner(System.in);
 		N = sc.nextInt();
 		
+		arr = new int[N];
 		for (int i = 0; i < N; i++)
 			arr[i] = sc.nextInt();
 		
@@ -28,6 +29,22 @@ public class S10972 {
 		while (i > 0 && a[i-1] >= a[i]) i-= 1;
 		if (i <= 0) return false; // 마지막 순열
 		
+		int j = n-1;
+		while (a[j] <= a[i-1]) j-=1;
+		swap(i-1, j);
+		j = n-1;
+		while (i < j) {
+			swap(i, j);
+			i += 1;
+			j -= 1;
+		}
+		
 		return true;
+	}
+	
+	public static void swap(int a, int b) {
+		int tmp = arr[a];
+		arr[a] = arr[b];
+		arr[b] = tmp;
 	}
 }
